6215b9032909a217509e46372b61f40dbf089f5f
Jeremy Eckstrom <jeremyeckstrom11@gmail.com>
2020-10-21 19:59:43 -0400

    Add project files.

---
+++ StocksProject/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StocksProject.Models;
+
+namespace StocksProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Random()
+        {
+            ViewData["Message"] = "Your stock profile";
+            var myStock = new Stock()
+            {
+                Id = 1,
+                Name = "GOOG",
+                Price = "$23.99"
+            };
+
+            return View(myStock);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
